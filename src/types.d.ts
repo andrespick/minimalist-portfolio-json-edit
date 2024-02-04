a7d122954db065c861134850cc1f96a0954fb293
andrespick <andresfelipevelaflorez@gmail.com>
2024-02-03 22:48:35 -0500

    modificar el json

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,57 @@
 export type SocialIcon = Record<string, string | any>
+
+/*
+{
+  "name": "AdventJS",
+  "isActive": true,
+  "description": "Retos de programación en JavaScript para cada día de diciembre hasta Navidad",
+  "highlights": [
+    "+1 millón de visitas",
+    "Multidioma",
+    "JavaScript & TypeScript"
+  ],
+  "url": "https://advent.js/dev"
+},
+{
+  "name": "Codember",
+  "isActive": true,
+  "description": "Juego interactivo de programación con retos y secretos",
+  "highlights": [
+    "+2000 usuarios activos",
+    "Retos para cualquier lenguaje"
+  ],
+  "url": "https://codember.dev"
+},
+{
+  "name": "codi.link",
+  "isActive": true,
+  "description": "Playground de código HTML, CSS y JavaScript para compartir",
+  "highlights": [
+    "100k visitas al mes",
+    "Desarrollado por la comunidad"
+  ],
+  "url": "https://codi.link",
+  "github": "https://github.com/midudev/codi.link"
+},
+{
+  "name": "Pruebas Técnicas",
+  "isActive": true,
+  "description": "Lista de pruebas técnicas de empresas para practicar programación",
+  "highlights": [
+    "100% gratuito",
+    "+500 soluciones de la comunidad"
+  ],
+  "url": "https://pruebastecnicas.com",
+  "github": "https://github.com/midudev/pruebas-tecnicas"
+},
+{
+  "name": "miduConf",
+  "description": "Conferencia de programación en streaming para la comunidad hispanohablante con charlas top",
+  "highlights": [
+    "+15k tickets repartidos",
+    "+50k espectadores en directo"
+  ],
+  "url": "https://miduconf.com"
+  
+}
+*/
